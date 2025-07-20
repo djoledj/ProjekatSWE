05ee4e0202f13bcf48e75e68f5e799e7a6953706
Djordje <djordje.djokic@elfak.rs>
2025-07-21 01:55:36 +0200

    nesto sitno

---
+++ backend/Controllers/FilmController.cs
@@ -0,0 +1,37 @@
+namespace WebTemplate.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmController : ControllerBase
+{
+  public DjoleGrandContext Context { get; set; }
+
+  public FilmController(DjoleGrandContext context)
+  {
+    Context = context;
+  }
+
+  [HttpGet("/film")]
+  public ActionResult GetFilmovi()
+  {
+    try
+    {
+      var films = Context.Filmovi!.Select(p => new
+      {
+        p.ID,
+        p.Naziv,
+        p.Description,
+        p.Reziser
+      }).ToList();
+
+      return Ok(films);
+    }
+    catch (Exception ec)
+    {
+      return BadRequest(ec.Message);
+    }
+
+  }
+
+
+}
